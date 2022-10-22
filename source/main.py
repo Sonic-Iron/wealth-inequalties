4ea78d88d64ba93ca4894a3a03b872556e2125a9
Sonic-Iron <sh2611@york.ac.uk>
2022-10-22 14:48:00 +0100

    Added code from previous attempt and refactored to improve it

--- source/main.py
+++ source/main.py
@@ -1,43 +1,69 @@
+"""
+Runs a yard sale simulation
+"""
 import random
-
-players = []
-num_players = 2
-num_rounds = 1000
-num_transactions_PRound = 1
-PercentWagedLarge = 0.2
-PercentWagedSmall = 0.17
-LargeBias = 0.1
-
-starting_wealth = 200
 
 
 class Player:
-    def __init__(self, ID, wealth):
-        self.ID = ID
-        self.wealth = wealth
+    """
+    Represents a player
+    """
+    def __init__(self, wealth):
+        """
+        Create a player
+        :param wealth: the player's wealth
+        """
+        self._wealth = wealth
 
-    def lose(self, wtl):
-        self.wealth -= wtl
+    def lose(self, loss):
+        """
+        :param loss: how much wealth was lost
+        """
+        self._wealth -= loss
 
-    def win(self, wtw):
-        self.wealth += wtw
-def generateplayers():
-    for x in range(num_players):
-        players.append(Player(len(players), starting_wealth))
+    def win(self, gain):
+        """
+        :param gain: how much wealth was won
+        """
+        self._wealth += gain
 
 
-def runtransaction(players):
-    P1 = random.choice(players)
-    P2 = P1
-    while(P2==P1):
-        P2 = random.choice(players)
-    if random.randint(0,1):
-        pass
+def generate_players(count, wealth):
+    """
+    Generates a pool of players
+    :param count: the number of players
+    :param wealth: the starting wealth of each player
+    :return: list of Player()
+    """
+    return [Player(wealth) for _ in range(count)]
+
+
+def run_round(players):
+    """
+    Run a round of the game
+    :param players: the list of players, len >= 2
+    """
+    p1, p2 = random.sample(players, 2)
+
+    # TODO: replace with random distribution code
+
 
 def main():
-    generateplayers()
-    runtransaction(players)
+    """
+    Runs the game
+    """
+    num_players = 2
+    num_rounds = 1000
+    # transactions_per_round = 1
+    # large_wager = 0.2
+    # small_wager = 0.17
+    # large_bias = 0.1
+    starting_wealth = 200
 
+    players = generate_players(num_players, starting_wealth)
+
+    for _ in range(num_rounds):
+        run_round(players)
 
 
 if __name__ == "__main__":
