f50e2a7e04a04dae986619436cbf0a4ef8b3964b
Sonic-Iron <sh2611@york.ac.uk>
2022-10-25 21:20:09 +0100

    Changed wealth distribution algorithm increased size of starting wealth for more precision

--- source/main.py
+++ source/main.py
@@ -1,7 +1,6 @@
 """
 Runs a yard sale simulation
 """
-from decimal import Decimal
 import random
 import numpy as np
 
@@ -15,28 +14,7 @@ class Player:
         Create a player
         :param wealth: the player's wealth
         """
-        self._wealth = Decimal(wealth)
-
-    def get_wealth(self):
-        """
-        The current level of wealth
-        :return: floating point representation of the wealth of the player
-        """
-        return self._wealth
-
-    def lose(self, loss):
-        """
-        Wealth lost
-        :param loss: how much wealth was lost
-        """
-        self._wealth -= Decimal(loss)
-
-    def win(self, gain):
-        """
-        Wealth gained
-        :param gain: how much wealth was won
-        """
-        self._wealth += Decimal(gain)
+        self.wealth = wealth
 
 
 def generate_players(count, wealth):
@@ -49,56 +27,52 @@ def generate_players(count, wealth):
     return [Player(wealth) for _ in range(count)]
 
 
-def run_round(players, large_bias, small_wager, large_wager):
+def run_round(p1, p2, large_bias, small_wager, large_wager):
     """
     Run a round of the game
-    :param players: the list of players, len >= 2
+    :param p1: the list of players, len >= 2
     :param large_bias: The percent of time the wealthier player will win
     :param small_wager: The percent won/lost when the poorer player wins
     :param large_wager: The percent won/lost when the richer player wins
     """
-    players_temp = players[:]
-    for _ in range(len(players)//2):
-        p1, p2 = random.sample(players_temp, 2)
-        players_temp.remove(p1)
-        players_temp.remove(p2)
-        if np.random.binomial(1, 0.5 + large_bias):
-            # the wealthier player wins
-            if p1.get_wealth() >= p2.get_wealth():
-                p1.win(p2.get_wealth()*Decimal(small_wager))
-                p2.lose(p2.get_wealth()*Decimal(small_wager))
-            else:
-                p2.win(p1.get_wealth()*Decimal(small_wager))
-                p1.lose(p1.get_wealth()*Decimal(small_wager))
-        else:
-            # the poorer player wins
-            if p1.get_wealth() >= p2.get_wealth():
-                p1.lose(p2.get_wealth()*Decimal(large_wager))
-                p2.win(p2.get_wealth()*Decimal(large_wager))
-            else:
-                p1.win(p1.get_wealth()*Decimal(large_wager))
-                p2.lose(p1.get_wealth()*Decimal(large_wager))
+    p_more, p_less = ((p1, p2) if p1.wealth >= p2.wealth else (p2, p1))
+    if np.random.binomial(1, 0.5 + large_bias):
+        # the wealthier player wins
+        wager = int(p_less.wealth * small_wager)
+        p_more.wealth += wager
+        p_less.wealth -= wager
+    else:
+        wager = int(p_less.wealth * large_wager)
+        p_less.wealth += wager
+        p_more.wealth -= wager
+
+
+def generate_pairs(players):
+    permutation = np.random.permutation(players)
+    for i in range(0, len(players), 2):
+        yield permutation[i], permutation[i+1]
 
 
 def main():
     """
     Runs the game
     """
-    num_players = 2
-    num_rounds = 1000000
+    num_players = 64
+    num_rounds = 1000
     large_wager = 0.2
     small_wager = 0.17
     large_bias = 0.01
-    starting_wealth = 200
+    starting_wealth = 200000
 
     players = generate_players(num_players, starting_wealth)
 
     for a in range(num_rounds):
-        run_round(players, large_bias, small_wager, large_wager)
+        for p1, p2 in generate_pairs(players):
+            run_round(p1, p2, large_bias, small_wager, large_wager)
         total_wealth = 0
         for b in players:
-            total_wealth += b.get_wealth()
-        print("round", a, players[0].get_wealth(), players[1].get_wealth(), total_wealth)
+            total_wealth += b.wealth
+        print("round", a, [p.wealth for p in players], total_wealth)
 
 
 if __name__ == "__main__":
