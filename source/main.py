a387fe45c35e88087895b77b79f2c4a46025c8c5
Sonic-Iron <sh2611@york.ac.uk>
2022-11-24 14:53:07 +0000

    got alot data. EZ GG

--- source/main.py
+++ source/main.py
@@ -9,7 +9,12 @@ def main():
     main
     :return: None
     """
-    run_sim()
+    large_wager_array = [0.2, 0.21, 0.25]
+    small_wager_array = [0.17, 0.17, 0.1]
+    large_bias_array = [0, 0.1, 0.2, 0.3, 0.4, 0.5]
+    for i in range(len(large_wager_array)):
+        for j in range(len(large_bias_array)):
+            run_sim(large_wager=large_wager_array[i], small_wager=small_wager_array[i], large_bias=large_bias_array[j])
 
 
 if __name__ == "__main__":
