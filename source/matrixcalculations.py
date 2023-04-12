4ea5d361bdc8bf271fd56e3b010b8fccd4be4bc6
Sonic-Iron <sh2611@york.ac.uk>
2023-04-13 00:57:45 +0100

    created an initial c

--- source/matrixcalculations.py
+++ source/matrixcalculations.py
@@ -1,38 +1,42 @@
-import numpy as np
-from scipy import linalg
-
-
-
-
 def main():
-    N = 32
     agentCount = 32
+
+    N = 32
+
     numRounds = 100
-    K = 1000
+
     startingWealth = 1000
+
+
     W = agentCount*startingWealth
-    maxWealth = agentCount*startingWealth
+    K = agentCount*startingWealth
+
+
     h = W/K+1
+
     P_his = []
-    P_his.append(create_c(startingWealth,agentCount))
+    P_his.append(create_c(startingWealth, agentCount, K))
+    print(P_his)
     gamma = 0.2
     tau = 0.02
-    enter_loop(numRounds, P_his, W, N, gamma, tau)
+    #enter_loop(numRounds, P_his, W, N, gamma, tau)
 
-def create_c(startingWealth, agentCount):
+def create_c(startingWealth,agentCount, K):
     temp = []
-    for _ in range(agentCount):
-        temp.append(startingWealth)
+    for _ in range(K):
+        temp.append(0)
+    temp[startingWealth-1] = agentCount
     return temp
 
-def enter_loop(numRounds, P_his,W, N, gamma, tau):
+def enter_loop(numRounds, P_his, W, N, gamma, tau):
     return ''
 
-def new_matrix():
+def new_P():
     return ''
 
-def new_A():
-    return ''
+def new_A(i, h, N, c,):
+    term1 = (h*c[i+1])
+    return
 
 def new_B():
     return ''
