21d443d458cef2686b98936e4bebe34355da135e
Sonic-Iron <sh2611@york.ac.uk>
2023-04-13 18:58:40 +0100

    writing P function

--- source/matrixcalculations.py
+++ source/matrixcalculations.py
@@ -1,11 +1,10 @@
 def main():
-    agentCount = 64
+    N = agentCount = 2
 
-    N = 32
 
     numRounds = 100
 
-    startingWealth = 1000
+    startingWealth = 5
 
 
     W = agentCount*startingWealth
@@ -15,35 +14,47 @@ def main():
 
     P_his = []
     P_his.append(create_c(startingWealth, agentCount, K))
-    gamma = 0.2
+    beta = 0.2
     tau = 0.02
-    enter_loop(numRounds, P_his, W, N, gamma, tau, K)
-
+    P_his = enter_loop(numRounds, P_his, h, N, c, K, W, N, beta, tau)
+    print(P_his)
 def create_c(startingWealth,agentCount, K):
     temp = []
-    for _ in range(K+1):
+    for _ in range(K+3):
         temp.append(0)
     temp[startingWealth] = agentCount
+    print(temp)
     return temp
 
-def enter_loop(numRounds, P_his, W, N, gamma, tau, K):
-    for i in range(0, K-1):
-        pass
+def enter_loop(numRounds, P_his, h, N, c, K, W, N, beta, tau):
+    for _ in range(numRounds):
+        P_his.append(next_c(h, N, c, K, W, N, beta, tau))
+    return P_his
 
-def new_P():
+def next_c(h, N, c, K, W, N, beta, tau):
+    old_c = c
+    new_c = c
+    for i in range(1, K+1):
+        new_c[i]= new_P(i, h, N, old_c, K, W, N, beta, tau)
+    return new_c
+def new_P(i, h, N, c, K, W, N, beta, tau):
+    term1 = (1 - ((2/(h^2))*(beta^2)*(((i*h)^2)/2)))
     return ''
 
 def new_A(i, h, N, c, K):
     term1 = (h*c[i])/(2*N)
     term2 = 0
-    for a in range(i+1, K+1):
-        term2 += c[a]
+    for n in range(i+1, K+1):
+        term2 += c[n]
     term2 = term2*(h/N)
     return term1 + term2
 
 def new_B(h, N, i, c):
     term1 = ((h/(2*N))*(((h^2)*((2*i)-1))-h))*c[i]
     term2 = 0
-
+    for n in range(1,i):
+        term2 += ((4*(h^2))-(2*h))*c[n]
+    term2 = term2*(h/(2*N))
+    return term1 + term2
 
 main()
