55260369a4f62a20543405520b0177ab3432a50e
Sonic-Iron <sh2611@york.ac.uk>
2023-05-02 21:50:03 +0100

    rewriting B done

--- source/matrixcalculations.py
+++ source/matrixcalculations.py
@@ -2,7 +2,7 @@ from GameInstance import generate_gini_coefficient
 from decimal import Decimal
 
 def main():
-    N = agentCount = 4
+    N = agentCount = 2
     numRounds = 1000
     startingWealth = 5
     W = agentCount*startingWealth
@@ -39,7 +39,6 @@ def new_P(i, h, N, c, K, W, beta, tau):
     term1b = new_B(h, N, i, c)
     ih_squ_div2 = ((i*h)**2)/2
     term1 = (1 - (constant_hb*((ih_squ_div2*term1a)+term1b)))*c[i]
-    assert term1 >= 0
     term2newa = new_A((i+1), h, N, c, K)
     term2newb = new_B(h, N, (i-1), c)
     term2 = (1/(2*(h**2)))*((2*(beta**2)*((((((i+1)*h)**2)/2)*term2newa)+term2newb)) - (h*tau*((W/N)-((i+1)*h))))*c[i+1]
@@ -61,7 +60,7 @@ def new_B(h, N, i, c):
 
     term2 = 0
     for n in range(1, i):
-        term2 += (9*(i**2) - 3*i + 1)*c[n]
+        term2 += 6*(i**3) - 9*(i**2) + 9*i - 1
     term2 *= 4
     return (term1 + term2)*h_constant
 
