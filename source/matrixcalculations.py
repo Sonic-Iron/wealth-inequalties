1c9231b6812457b25040a378e3b7b7c487f03451
Sonic-Iron <sh2611@york.ac.uk>
2023-05-07 03:40:44 +0100

    creating a gini matrix

--- source/matrixcalculations.py
+++ source/matrixcalculations.py
@@ -2,18 +2,18 @@ from GameInstance import generate_gini_coefficient
 from decimal import Decimal
 
 def main():
-    N = agentCount = 32
-    numRounds = 100000
+    N = agentCount = 4
+    numRounds = 1000
     startingWealth = 3
     W = agentCount*startingWealth
-    K = int((W-1))
+    K = int((W-1)*0.8)
     h = W/(K+1)
     P_his = []
     c = create_c(startingWealth, agentCount, K)
     beta = 0.2
     tau = 0.0
     P_his, G_his = enter_loop(startingWealth, numRounds, P_his, h, c, K, W, N, beta, tau)
-
+    print(G_his)
 def create_c(startingWealth,agentCount, K):
     temp = [0]*(K+2)
     temp[startingWealth] = agentCount
@@ -32,7 +32,7 @@ def next_c(h, c, K, W, N, beta, tau):
         new_c[i] = new_P(i, h, N, c, K, W, beta, tau)
     for a in range(len(new_c)):
         new_c[a] = N * new_c[a]/sum(new_c)
-    print(sum(new_c), sum([p*c[p] for p in range(len(new_c))]), new_c)
+    #print(sum(new_c), sum([p*c[p]*h for p in range(len(new_c))]), new_c)
     return new_c
 def new_P(i, h, N, c, K, W, beta, tau):
     constant_hb = (2/(h**2))*(beta**2)
@@ -64,20 +64,19 @@ def new_B(h, N, i, c):
 
 def gen_gini_matrix(W, c, h):
     area = 0
-    total_wealth = sum([p*c[p] for p in range(len(c))])
+    total_wealth = sum([p*c[p]*h for p in range(len(c))])
 
     prop_pop_pre = 0
-    prop_wealth = 0
-    for popnum in range(len(c)):
-        popnum_pop = c[popnum]/sum(c) - prop_pop_pre
-        prop_pop_pre += c[popnum]/sum(c)
-
-
-
-
-
-
-    return area
+    prop_wealth_pre = 0
+    for num in range(len(c)):
+        num_pop_percent = c[num]/sum(c)
+        popnum_wealth_percent = (c[num] * num * h)/total_wealth
+        area += (num_pop_percent * popnum_wealth_percent/2) + (prop_pop_pre * num_pop_percent)
+        prop_wealth_pre += popnum_wealth_percent
+    A = 0.5 - area
+    B = area
+    gini = A/(A+B)
+    return gini
 
 
 main()
